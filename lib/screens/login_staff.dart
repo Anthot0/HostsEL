78b4e29ac1b680ba5f545197a0170cdf1063cd19
Antot0 <ohayon.anthony@gmail.com>
2025-02-25 23:14:49 +0700

    Corrections et mise à jour

--- lib/screens/login_staff.dart
+++ lib/screens/login_staff.dart
@@ -1,68 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hosts_el/screens/dashboard.dart';
-import 'package:hosts_el/screens/home_screen.dart';
 
-class LoginStaffScreen extends StatefulWidget {
+class LoginStaffScreen extends StatelessWidget {
   const LoginStaffScreen({super.key});
-
-  @override
-  _LoginStaffScreenState createState() => _LoginStaffScreenState();
-}
-
-class _LoginStaffScreenState extends State<LoginStaffScreen> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  Future<void> _login() async {
-    try {
-      await _auth.signInWithEmailAndPassword(
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),
-      );
-
-      if (!mounted) return; // Évite les erreurs si le widget est détruit
-
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const DashboardScreen()),
-      );
-    } catch (e) {
-      if (!mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Erreur de connexion : ${e.toString()}")),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Staff'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: "Email"),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: "Mot de passe"),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _login,
-              child: const Text("Connexion"),
-            ),
-          ],
+      appBar: AppBar(title: const Text('Connexion Staff')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const DashboardScreen()),
+            );
+          },
+          child: const Text('Se connecter'),
         ),
       ),
     );
